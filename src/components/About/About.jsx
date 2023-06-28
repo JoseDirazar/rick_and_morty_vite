cc41e3f5f3a1207ef7b19438a7f56836b7354ea5
Jo <jose_f_dirazar@hotmail.com>
2023-06-28 11:30:05 -0300

    que quilombo con git loco

--- src/components/About/About.jsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <div className="divAbout">
-      <h1>Simulacion de About</h1>
-    </div>
-  );
-}
--- src/components/Error404/Error404.jsx
+++
@@ -1,5 +0,0 @@
-export default function Error404() {
-    return <div>
-        <h1>Pagina no encontrada</h1>
-    </div>
-}
